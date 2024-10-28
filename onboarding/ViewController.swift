7f38f9e303fd129c77d3de5e6078331cb31e0780
Trung <tttrung2101@gmail.com>
2024-10-28 17:27:55 +0700

    Initial Commit

---
+++ onboarding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onboarding
+//
+//  Created by Trung on 24/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var view1: UIView!
+    @IBOutlet var view2: UIView!
+    @IBOutlet var view3: UIView!
+    
+    @IBOutlet weak var btnSkip: UIButton!
+    @IBOutlet weak var btnNext: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
